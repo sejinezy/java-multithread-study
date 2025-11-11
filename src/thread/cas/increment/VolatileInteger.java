fc451ab22ba97e13d01145426eeaf4717544f7ff
sejin <psj8880@naver.com>
2025-11-11 13:54:32 +0900

    feat(VolatileInteger): volatile 적용
    - volatile을 사용하면 CPU의 캐시 메모리를 무시하고, 메인 메모리를 직접 사용하도록 한다.
    - 이 문제는 연산 자체가 나누어져 있기 때문에 발생한다. volatile은 연산 자체를 묶어주는 기능이 아니다.

---
+++ src/thread/cas/increment/VolatileInteger.java
@@ -0,0 +1,17 @@
+package thread.cas.increment;
+
+public class VolatileInteger implements IncrementInteger{
+
+    private volatile int value;
+
+    @Override
+    public void increment() {
+        value++;
+    }
+
+    @Override
+    public int get() {
+        return value;
+    }
+
+}
