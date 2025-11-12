e05a5501b128ab0aab7382e5f5a4ef51748e7917
sejin <psj8880@naver.com>
2025-11-12 17:52:23 +0900

    feat(ExecutorUtils): Executor 프레임워크의 상태를 확인하기 위한 로그 출력 유틸리티 구현
    - pool : 스레드 풀에서 관리되는 스레드의 숫자
    - active: 작업을 수행하는 스레드의 숫자
    - queuedTasks : 큐에 대기중인 작업의 숫자
    - completedTask: 완료된 작업의 숫자
    - printState() 메서드에 ThreadPoolExecutor 구현체가 넘어오면 우리가 구성한 로그를 출력하고, 그렇지 않은 경우에는 인스턴스 자체를 출력한다.

---
+++ src/thread/executor/ExecutorUtils.java
@@ -0,0 +1,23 @@
+package thread.executor;
+
+import static util.MyLogger.log;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public abstract class ExecutorUtils {
+
+    public static void printState(ExecutorService executorService) {
+        if (executorService instanceof ThreadPoolExecutor poolExecutor) {
+            int pool = poolExecutor.getPoolSize();
+            int active = poolExecutor.getActiveCount();
+            int queuedTasks = poolExecutor.getQueue().size();
+            long completedTask = poolExecutor.getCompletedTaskCount();
+            log("[pool=" + pool + ", active=" + active + ", queuedTasks=" + queuedTasks + ", completedTasks="
+                    + completedTask + "]");
+
+        } else{
+            log(executorService);
+        }
+    }
+}
