b6d5346b00be12f99af11f73a78376a791354e69
sejin <psj8880@naver.com>
2025-11-13 18:51:56 +0900

    feat(PrestartPoolMain): 스레드 미리 생성하기
    - 응답 시간이 아주 중요한 서버라면, 서버가 고객의 처음 요청을 받기 전에 스레드를 스레드 풀에 미리 생성해두고 싶을 수 있다.
    - 스레드를 미리 생성해두면, 처음 요청해서 사용되는 스레드의 생성 시간을 줄일 수 있다.
    - ThreadPoolExecutor.prestartAllCoreThreads()를 사용하면 기본 스레드를 미리 생성할 수 있다.

---
+++ src/thread/executor/PrestartPoolMain.java
@@ -0,0 +1,19 @@
+package thread.executor;
+
+import static thread.executor.ExecutorUtils.*;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class PrestartPoolMain {
+
+    public static void main(String[] args) {
+        ExecutorService es = Executors.newFixedThreadPool(1000);
+
+        printState(es);
+        ThreadPoolExecutor poolExecutor = (ThreadPoolExecutor) es;
+        poolExecutor.prestartAllCoreThreads();
+        printState(es);
+    }
+}
