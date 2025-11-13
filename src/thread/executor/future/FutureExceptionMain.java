0532a1ce7cb66b9b4d48fa18b35b96f52c9f281e
sejin <psj8880@naver.com>
2025-11-13 14:24:35 +0900

    feat(FutureExceptionMain): Future 예외
    - Future.get()을 호출하면 작업의 결과값 뿐만 아니라, 작업 중에 발생한 예외도 받을 수 있다.
    - Future의 상태가 FAILED면 ExecutionException 예외를 던진다.
    - e.getCause()을 호출하면 작업에서 발생한 원본 예외를 받을 수 있다.

---
+++ src/thread/executor/future/FutureExceptionMain.java
@@ -0,0 +1,42 @@
+package thread.executor.future;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class FutureExceptionMain {
+
+    public static void main(String[] args) {
+        ExecutorService es = Executors.newFixedThreadPool(1);
+        log("작업 전달");
+        Future<Integer> future = es.submit(new ExCallable());
+        sleep(1000); // 잠시 대기
+
+        try {
+            log("future.get() 호출 시도, future.state(): " + future.state());
+            Integer result = future.get();
+            log("result value = " + result);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            log("e = " + e);
+            Throwable cause = e.getCause();
+            log("cause = " + cause);
+        }
+        es.close();
+    }
+
+    static class ExCallable implements Callable<Integer>{
+
+        @Override
+        public Integer call() {
+            log("Callable 실행, 예외 발생");
+            throw new IllegalStateException("ex!");
+        }
+    }
+}
